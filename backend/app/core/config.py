c1f5137a0c1e586be08235f9bb89797b2f35452d
mleader <mleader@redhat.com>
2021-01-29 13:34:37 -0500

    add config validator and parser

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,7 +1,34 @@
 import os
 import sys
-import configparser
 from pathlib import Path
+import configparser
+
+from vyper import v
+from vyper.errors import UnsupportedConfigError
+from colorama import Fore, Back, Style
+import vyper
+from environs import Env
+from marshmallow.validate import Length
+from typing import Optional
+import os
+
+
+def vyper_cfg():
+	env = Env()
+	# env.read_env()
+	if os.getenv('OCPPERF_SERVER_CONFIG') is not None:
+		env.str(
+			'OCPPERF_SERVER_CONFIG',
+			validate=Length(min=1),
+			subset=Optional
+		)
+	v.set_config_name('ocpperf')
+	v.add_config_path('.')
+	v.add_config_path('./app')
+	print(v.debug())
+	return v.read_in_config()
+	
+	
 
 
 def get_server_config():
@@ -15,3 +42,7 @@ def get_server_config():
 	config.read(Path(config_path))
 
 	return config
+
+
+if __name__ == '__main__':
+	vyper_cfg()
