6b6fa319da41d74bd75fb5fdf20e4dc84a86cef3
Keith Whitley <kwhitley@redhat.com>
2021-04-12 11:41:59 -0400

    refactoring some of the frontend code continued again

--- frontend/src/components/PlatformTabs.js
+++ frontend/src/components/PlatformTabs.js
@@ -2,8 +2,8 @@ import ReactDOM from 'react-dom';
 import React, { useState, useEffect } from 'react';
 import { Tabs, Tab, TabTitleText, Checkbox } from '@patternfly/react-core';
 
+import VersionLinks from './VersionLinks';
 import PerformanceResults from './PerformanceResults';
-import VersionList from './PerformanceResults';
 
 class PlatformTabs extends React.Component {
   constructor(props) {
@@ -27,8 +27,8 @@ class PlatformTabs extends React.Component {
       <Tabs activeKey={activeTabKey} onSelect={this.handleTabClick}>
       {this.props.data.map((tab, index) => (         
         <Tab eventKey={index} title={<TabTitleText>{tab.title}</TabTitleText>}> 
+          <VersionLinks data={tab.data} />
           <PerformanceResults data={tab.data} />
-          <VersionList data={tab.data} />
          </Tab>
        ))
       }
