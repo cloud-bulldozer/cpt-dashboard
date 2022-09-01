0915c2c05b7e86b5e879822284cdd06fe55e7307
Joe Talerico (rook) <jtaleric@redhat.com>
2022-09-01 06:32:35 -0400

    Brining the dashbaord back to life (#12)
    Been out for a bit... Time to bring it back to life
    
    Signed-off-by: Joe Talerico (rook) <joe.talerico@gmail.com>
    
    Signed-off-by: Joe Talerico (rook) <joe.talerico@gmail.com>

--- frontend/src/components/Platform/Table/Cards/Upgrades.js
+++ frontend/src/components/Platform/Table/Cards/Upgrades.js
@@ -9,7 +9,7 @@ import { SiApacheairflow } from "react-icons/si";
 export default function UpgradeCard(props) {
     let upgradeConfig = props.data
 
-    const [isExpanded, setExpanded] = React.useState([true, null])
+    const [isExpanded, setExpanded] = React.useState([false, false])
 
     const icons = {
         "failed": <><FaExclamationCircle color="red"/></>,
@@ -17,7 +17,6 @@ export default function UpgradeCard(props) {
         "upstream_failed": <><FaExclamationTriangle color="yellow"/> Upstream Failed </>,
 
     }
-
 
     const onExpand = () => {
         setExpanded(!isExpanded)
@@ -41,21 +40,32 @@ export default function UpgradeCard(props) {
                 <CardExpandableContent>
                 <CardBody>
                 <ul>
-                             
                     <li> {icons[upgradeConfig.job_status] || upgradeConfig.job_status}
                      { upgradeConfig.job_status == "success" ? upgradeConfig.cluster_version : ""} ({upgradeConfig.job_status != "upstream_failed" ? formatTime(upgradeConfig.job_duration) : "Skipped" })</li>    
-                     <a href={upgradeConfig.build_url}><SiApacheairflow color="teal"/></a>              
-                        
+                     <a href={upgradeConfig.build_url}><SiApacheairflow color="teal"/></a>
 
-                </ul> 
+                </ul>
                 </CardBody>
                 </CardExpandableContent>
             </Card>)
     } else {
         return (
-            <Card>
+            <Card isHoverable isExpanded={isExpanded}>
+                <CardHeader
+          onExpand={onExpand}
+          toggleButtonProps={{
+            id: 'toggle-button',
+            'aria-label': 'Details',
+            'aria-labelledby': 'titleId toggle-button',
+            'aria-expanded': isExpanded
+          }}
+        >
+                    <CardTitle>Upgrades</CardTitle>
+                </CardHeader>
                 <CardTitle>Upgrades</CardTitle>
-                <CardBody><Spinner isSVG /><br />Awaiting Results</CardBody>
+                <CardExpandableContent>
+                <CardBody><br />No Upgrade Results</CardBody>
+                </CardExpandableContent>
                 <CardFooter></CardFooter>
             </Card>
         )
