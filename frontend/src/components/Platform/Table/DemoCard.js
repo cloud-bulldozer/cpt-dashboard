ee1fd9599f9e86ef965e24e89085a61ba8f90e01
Vicente Zepeda Mas <vzepedam@redhat.com>
2023-09-20 16:23:16 +0200

    HotFix: disables Graph card section
    Missing Graph file for it to work
    
    Signed-off-by: Vicente Zepeda Mas <vzepedam@redhat.com>

--- frontend/src/components/Platform/Table/DemoCard.js
+++ frontend/src/components/Platform/Table/DemoCard.js
@@ -3,7 +3,7 @@ import { Card, CardTitle, CardBody, CardFooter } from '@patternfly/react-core';
 import { Grid, GridItem } from '@patternfly/react-core';
 import useCardData from '../../../hooks/useCardData'
 import InstallCard from './Cards/Install'
-import GraphCard from './Cards/Graph'
+// import GraphCard from './Cards/Graph'
 import ScaleCard from './Cards/Scale'
 import UpgradeCard from './Cards/Upgrades'
 import BenchmarkCard from './Cards/Benchmarks'
@@ -24,9 +24,9 @@ export default function Basic(props) {
         <GridItem span="5">
 -          <BenchmarkCard data={ cardData.filter((item) => item.build_tag != "install") } />
         </GridItem>
-        <GridItem span="7">
+        {/* <GridItem span="7">
           <GraphCard data={ cardData[0] } />
-        </GridItem>
+        </GridItem> */}
       </Grid>
     )
   }
