e45982ee340095a34c71e14e3489858087a5d890
Matthew F Leader <mleader@redhat.com>
2023-11-28 16:13:16 -0500

    fix jira auth arg

--- backend/app/services/jira_svc.py
+++ backend/app/services/jira_svc.py
@@ -8,10 +8,10 @@ class JiraService:
     def __init__(self, configpath="jira"):
         self.cfg = config.get_config()
         self.url = self.cfg.get(configpath+'.url')
-        self.pat = self.cfg.get(configpath+'.access_token')
+        self.pat = self.cfg.get(configpath+'.personal_access_token')
         self.svc = Jira(
             url=self.url,
-            access_token=self.pat
+            token=self.pat
         )
 
     def jql(self, query: str, fields="*all", expand=None, validate_query=None):
@@ -21,3 +21,4 @@ class JiraService:
             expand=expand,
             validate_query=validate_query
         )
+    
