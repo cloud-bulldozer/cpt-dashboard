1fdce8c6e0552d96e39be22163121dba82338902
shubham-html-css-js <shubh8221a@gmail.com>
2023-12-08 18:27:05 +0530

    Change how we are populating product name for HCE results

--- backend/app/api/v1/endpoints/cpt/maps/hce.py
+++ backend/app/api/v1/endpoints/cpt/maps/hce.py
@@ -19,7 +19,8 @@ async def hceMapper(start_datetime: date, end_datetime: date):
     df = await getData(start_datetime, end_datetime)
     df["releaseStream"] = "Nightly"
     df["ciSystem"] = "Jenkins"
-    df["testName"] = df["test"]
+    df["testName"] = df["product"] + ":" + df["test"]
+    df["product"] = df["group"] 
     df["jobStatus"] = df['result'].apply(lambda x: "SUCCESS" if x == 'PASS' else "FAILURE")
     df["version"] = df['version'].apply(lambda x: x if len(x.split(":")) == 1 else x.split(":")[1][:7])
     df["uuid"] = df["result_id"]
