edec519d2ffc75e041d69df870c6502ced8fa719
MVarshini <varm@redhat.com>
2024-04-01 15:49:41 +0530

    PANDA-361 Revamp CPT dashboard

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
-
 export default defineConfig({
   plugins: [react()],
   esbuild: {
