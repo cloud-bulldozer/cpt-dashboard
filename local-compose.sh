70164e6b1f515b25d9a1e71bd8476dab123880ff
MVarshini <varm@redhat.com>
2024-05-14 22:53:11 +0530

    PANDA-406 OCP Page Layout

--- local-compose.sh
+++ local-compose.sh
@@ -11,7 +11,7 @@ CPT_CONFIG=${CPT_CONFIG:-"$PWD/backend/ocpperf.toml"}
 podman rm -f front back
 
 podman build -f backend/backend.containerfile --tag backend
-podman build -f frontend/frontend.containerfile --tag frontend
+# podman build -f frontend/frontend.containerfile --tag frontend
 
 # NOTE: add --network=host to test against a local containerized Horreum
 podman run -d --name=back -p ${CPT_BACKEND_PORT}:8000 --network=host -v "${CPT_CONFIG}:/backend/ocpperf.toml:Z" localhost/backend
