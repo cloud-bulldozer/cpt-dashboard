21799dc3a1b422b1eb76b9f322961fe324db1ed5
MVarshini <varm@redhat.com>
2024-06-13 22:46:04 +0530

    sidemenu toggle

--- frontend/src/reducers/sideMenuReducer.js
+++ frontend/src/reducers/sideMenuReducer.js
@@ -15,7 +15,7 @@ const SideMenuReducer = (state = initialState, action = {}) => {
         activeMenuItem: payload,
       };
     case TOGGLE_SIDE_MENU:
-      return { ...state, isSideMenuOpen: false };
+      return { ...state, isSideMenuOpen: payload };
     default:
       return state;
   }
