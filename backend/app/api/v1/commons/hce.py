7bebd8b6996950bf7da2d885b0772a9e4ab7ac9e
MVarshini <varm@redhat.com>
2024-11-09 17:19:02 +0530

    pagination params

--- backend/app/api/v1/commons/hce.py
+++ backend/app/api/v1/commons/hce.py
@@ -7,7 +7,9 @@ async def getData(
     start_datetime: date, end_datetime: date, size: int, offset: int, configpath: str
 ):
     query = {
-        "query": {"bool": {"filter": {"range": {"date": {"format": "yyyy-MM-dd"}}}}}
+        "size": size,
+        "from": offset,
+        "query": {"bool": {"filter": {"range": {"date": {"format": "yyyy-MM-dd"}}}}},
     }
     es = ElasticService(configpath=configpath)
     response = await es.post(
@@ -25,5 +27,4 @@ async def getData(
 
     jobs[["group"]] = jobs[["group"]].fillna(0)
     jobs.fillna("", inplace=True)
-
     return {"data": jobs, "total": response["total"]}
