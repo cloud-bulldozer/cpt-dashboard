2b7a1e5c7daa183ee12d7a3a0e98a41811adf9c9
David Butenhof <dbutenho@redhat.com>
2024-11-22 10:25:58 -0500

    Add a metric label template mechanism
    The API makes an effort to create unique metric labels when comparing data
    across multiple runs, but without human direction the constructed names aren't
    necessarily ideal to help a human focus on the comparisons they want to make.
    
    This PR adds a UI template mechanism to allow the human to format names that
    are more useful -- for example, to focus on the differences between software
    releases or hardware configurations expressed by Crucible tags or params.
    
    For example, a template of `<metric> <iteration>:<period> <tag:accelerator>`
    identifies metrics between two runs based on the hardware accelerator type
    used for each.
    
    This introduces a new action to fetch and store the available filters, which
    exposes the param and tag values currently in use. Usability of the template
    mechanism will depend on consistent application of specific tags, which we
    expect to be increasingly supplied automatically by Crucible discovery rather
    than relying on the (current) ad hoc user definition in endpoint files.

--- publish-containers.sh
+++ publish-containers.sh
@@ -17,23 +17,30 @@
 REGISTRY=${REGISTRY:-images.paas.redhat.com}
 ACCOUNT=${ACCOUNT:-${USER}}
 REPO=${REPO:-cpt}
-TAG=${TAG:-latest}
 SKIP_FRONTEND=${SKIP_FRONTEND:-0}
 SKIP_BACKEND=${SKIP_BACKEND:-0}
 
-REPOSITORY=${REGISTRY}/${ACCOUNT}/${REPO}
+REPOSITORY="${REGISTRY}/${ACCOUNT}/${REPO}"
+
+TAG=${TAG:-latest}
+BRANCH=$(git symbolic-ref --short HEAD)
+SHA1=$(git rev-parse --short=9 HEAD)
+REVISION="${BRANCH}-${SHA1}"
+TIMESTAMP=$(date +'%Y%m%d-%H%M%S')
+
 # remove current images, if any
 podman rm -f front back
-now=$(date +'%Y%m%d-%H%M%S')
 
 if [ "$SKIP_BACKEND" != 1 ] ;then
   podman build -f backend/backend.containerfile --tag backend
   podman push backend "${REPOSITORY}/backend:${TAG}"
-  podman push backend "${REPOSITORY}/backend:${now}"
+  podman push backend "${REPOSITORY}/backend:${TIMESTAMP}"
+  podman push backend "${REPOSITORY}/backend:${REVISION}"
 fi
 
 if [ "$SKIP_FRONTEND" != 1 ] ;then
   podman build -f frontend/frontend.containerfile --tag frontend
   podman push frontend "${REPOSITORY}/frontend:${TAG}"
-  podman push frontend "${REPOSITORY}/frontend:${now}"
+  podman push frontend "${REPOSITORY}/frontend:${TIMESTAMP}"
+  podman push frontend "${REPOSITORY}/frontend:${REVISION}"
 fi
