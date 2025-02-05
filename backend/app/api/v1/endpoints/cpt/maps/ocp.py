3ee907d34187de1c06e9b387724b16af3bdf9161
MVarshini <varm@redhat.com>
2025-02-05 11:45:27 +0530

    ocp filters

--- backend/app/api/v1/endpoints/cpt/maps/ocp.py
+++ backend/app/api/v1/endpoints/cpt/maps/ocp.py
@@ -1,7 +1,8 @@
 from app.api.v1.commons.ocp import getData, getFilterData
-from app.api.v1.commons.utils import getReleaseStream
+from app.api.v1.commons.utils import getReleaseStream, buildReleaseStreamFilter
 from datetime import date
 import pandas as pd
+from app.api.v1.commons.constants import keys_to_keep
 
 
 ################################################################
@@ -24,3 +25,40 @@ async def ocpMapper(
         df["testName"] = df["benchmark"]
         return {"data": df, "total": response["total"]}
     return {"data": pd.DataFrame(), "total": response["total"]}
+
+
+async def ocpFilter(start_datetime: date, end_datetime: date, filter: str):
+    response = await getFilterData(
+        start_datetime, end_datetime, filter, f"ocp.elasticsearch"
+    )
+
+    if isinstance(response, pd.DataFrame) or not response:
+        return {"total": 0, "filterData": [], "summary": {}}
+
+    if not response.get("filterData") or response.get("total", 0) == 0:
+        return {"total": response.get("total", 0), "filterData": [], "summary": {}}
+
+    # Normalize filters
+    for item in response["filterData"]:
+        if item.get("key") == "releaseStream":
+            current_values = item.get("value", [])
+            updated_values = buildReleaseStreamFilter(current_values)
+            item["value"] = updated_values
+        if item.get("key") == "benchmark":
+            item.update({"key": "testName", "name": "Test Name"})
+
+    # Add predefined filters
+    filters_to_add = [
+        {"key": "product", "name": "Product", "value": ["ocp"]},
+    ]
+    response["filterData"].extend(filters_to_add)
+
+    # Filter data based on allowed keys
+    filtered_data = [
+        item for item in response["filterData"] if item.get("key") in keys_to_keep
+    ]
+    return {
+        "total": response.get("total", 0),
+        "filterData": filtered_data,
+        "summary": response.get("summary", {}),
+    }
