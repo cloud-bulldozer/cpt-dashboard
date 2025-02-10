8ee65dee2cf7a87f9ae90f30aec0797018ca7ea9
MVarshini <varm@redhat.com>
2025-02-10 21:51:22 +0530

    build file

--- local-compose.sh
+++ local-compose.sh
@@ -11,11 +11,11 @@ CPT_CONFIG=${CPT_CONFIG:-"$PWD/backend/ocpperf.toml"}
 podman rm -f front back
 
 podman build -f backend/backend.containerfile --tag backend
-# podman build -f frontend/frontend.containerfile --tag frontend
+podman build -f frontend/frontend.containerfile --tag frontend
 
 # NOTE: add --network=host to test against a local containerized Horreum
 podman run -d --name=back -p ${CPT_BACKEND_PORT}:8000 --network=host -v "${CPT_CONFIG}:/backend/ocpperf.toml:Z" localhost/backend
 
-# podman run -d --name=front --net=host -p ${CPT_FRONTEND_PORT}:3000 localhost/frontend
+podman run -d --name=front --net=host -p ${CPT_FRONTEND_PORT}:3000 localhost/frontend
 
 
