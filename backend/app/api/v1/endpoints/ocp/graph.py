95a5bd4bdac3fd2608fd4dd7072ec4afe89aa7fe
MVarshini <varm@redhat.com>
2025-03-07 19:07:30 +0530

    head changes

--- backend/app/api/v1/endpoints/ocp/graph.py
+++ backend/app/api/v1/endpoints/ocp/graph.py
@@ -291,11 +291,7 @@ async def jobSummary(uuids: list):
     es = ElasticService(configpath="ocp.elasticsearch", index=index)
     response = await es.post(query=query)
     await es.close()
-<<<<<<< HEAD
     runs = [item["_source"] for item in response["data"]]
-=======
-    runs = [item["_source"] for item in response]
->>>>>>> e99c687 (PANDA-690)
     return runs
 
 
@@ -428,11 +424,7 @@ async def getBurnerResults(uuid: str, uuids: list, index: str, metric='podLatenc
     es = ElasticService(configpath="ocp.elasticsearch", index=index)
     response = await es.post(query=query)
     await es.close()
-<<<<<<< HEAD
     runs = [item["_source"] for item in response["data"]]
-=======
-    runs = [item["_source"] for item in response]
->>>>>>> e99c687 (PANDA-690)
     return runs
 
 
@@ -446,11 +438,7 @@ async def getResults(uuid: str, uuids: list, index: str):
     es = ElasticService(configpath="ocp.elasticsearch", index=index)
     response = await es.post(query=query)
     await es.close()
-<<<<<<< HEAD
     runs = [item["_source"] for item in response["data"]]
-=======
-    runs = [item["_source"] for item in response]
->>>>>>> e99c687 (PANDA-690)
     return runs
 
 
@@ -505,11 +493,7 @@ async def getMatchRuns(meta: dict, workerCount: False):
     es = ElasticService(configpath="ocp.elasticsearch")
     response = await es.post(query=query)
     await es.close()
-<<<<<<< HEAD
     runs = [item["_source"] for item in response["data"]]
-=======
-    runs = [item["_source"] for item in response]
->>>>>>> e99c687 (PANDA-690)
     uuids = []
     for run in runs:
         uuids.append(run["uuid"])
