7cade8af664927a9adf844ac0c45febadf2eab67
Matthew F Leader <mleader@redhat.com>
2025-03-15 18:16:04 -0400

    add other relevant files
    Signed-off-by: Matthew F Leader <mleader@redhat.com>

--- frontend/cypress/e2e/home.cy.js
+++ frontend/cypress/e2e/home.cy.js
@@ -3,15 +3,13 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit(url_gui)
     const nav_list_tab_name = ["OCP", "Quay", "Telco", "Home"];
-    // cy.get('[data-ouia-component-type="PF5/NAV"]');
+    cy.findByTestId("nav_list").each(($el) => {
+      nav_list_tab_name.forEach((tab_name) => {
+        cy.wrap($el).findByText(tab_name).should("exist").click();
+      })
+    });
 
-    // cy.findByTestId("nav_list").each(($el) => {
-    //   nav_list_tab_name.forEach((tab_name) => {
-    //     cy.wrap($el).findByText(tab_name).should("exist").click();
-    //   })
-    // });
-
-        // close sidebar masthead with hamburger toggle
+    // close sidebar masthead with hamburger toggle
     cy.get('[data-ouia-component-id="main_layout_toggle"]').should("exist").click();
     
     // cannot click summary b/c it requires a real user event
