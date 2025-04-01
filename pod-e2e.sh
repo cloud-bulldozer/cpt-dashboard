e4071ca1dad7a9f6fde67798b19c0f58bde51bc4
Matthew F Leader <mleader@redhat.com>
2025-04-01 09:55:38 -0400

    edit containerfiles
    Signed-off-by: Matthew F Leader <mleader@redhat.com>

--- pod-e2e.sh
+++ pod-e2e.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "building backend test image"
-podman build -f backend/tests/functional.containerfile --tag e2e-backend ./backend
+podman build -f backend/tests/e2e_backend.containerfile --tag e2e-backend ./backend
 echo "building frontend test image"
-podman build -f frontend/test_frontend.containerfile --tag e2e-frontend ./frontend
+podman build -f frontend/e2e_frontend.containerfile --tag e2e-frontend ./frontend
 
 POD_NAME="pod_e2e"
 
@@ -15,4 +15,4 @@ POD_NAME="pod_e2e" ./backend/tests/opensearch_ocp.sh
 podman run -d --pod=${POD_NAME} --name=back e2e-backend
 echo "seeding db"
 podman exec back poetry run python tests/db_seed.py
-podman run --rm -it --pod=${POD_NAME} --name=front e2e-frontend 
+podman run --rm -it --pod=${POD_NAME} --name=front e2e-frontend
