ee4eb8dc4dacf4bf186d5bebab996e5447978aa7
Matthew F Leader <mleader@redhat.com>
2025-04-01 15:45:49 -0400

    try it in a pod
    Signed-off-by: Matthew F Leader <mleader@redhat.com>

--- pod-e2e.sh
+++ pod-e2e.sh
@@ -15,12 +15,12 @@ set -e
 # echo "seeding db"
 # podman exec back poetry run python tests/db_seed.py
 
-./backend/tests/e2e_backend.sh
+# ./backend/tests/e2e_backend.sh
 
-# echo "building frontend test image"
-# podman build -f frontend/e2e_frontend.containerfile --tag e2e-frontend ./frontend
+echo "building frontend test image"
+podman build -f frontend/e2e_frontend.containerfile --tag e2e-frontend ./frontend
 
-npm run --prefix frontend cypress:run:ci
+# npm run --prefix frontend cypress:run:ci
 
-# podman run --rm -it --pod=${POD_NAME} --name=front e2e-frontend
-# exit $?
+podman run --rm -it --pod=${POD_NAME} --name=front e2e-frontend
+exit $?
