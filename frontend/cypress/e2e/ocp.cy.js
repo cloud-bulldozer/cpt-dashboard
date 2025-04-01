be5bc1d26383eec8fd3ce3c5f22556b4aa9fa1ff
Matthew F Leader <mleader@redhat.com>
2025-04-01 16:46:49 -0400

    comment out test code
    Signed-off-by: Matthew F Leader <mleader@redhat.com>

--- frontend/cypress/e2e/ocp.cy.js
+++ frontend/cypress/e2e/ocp.cy.js
@@ -55,42 +55,42 @@ describe('ocp user journey', () => {
     
     cy.screenshot("main-data-table")
 
-    cy.findByTestId("main_data_table")
-      .get("tbody")
-      .find(`[data-ouia-component-type="PF5/TableRow"]`, 
-          {hidden: false})
-      .as("table_row")
-      .first();
+    // cy.findByTestId("main_data_table")
+    //   .get("tbody")
+    //   .find(`[data-ouia-component-type="PF5/TableRow"]`, 
+    //       {hidden: false})
+    //   .as("table_row")
+    //   .first();
 
-    // in the list of table row elements, each table row is 
-    // paired with a subsequent hidden row that can be expanded
-    // to display details
-    cy.get("@table_row")
-      .next()
-      .as("expandable_rows")        
-      .should("not.be.visible");
+    // // in the list of table row elements, each table row is 
+    // // paired with a subsequent hidden row that can be expanded
+    // // to display details
+    // cy.get("@table_row")
+    //   .next()
+    //   .as("expandable_rows")        
+    //   .should("not.be.visible");
 
-    cy.get("@table_row")
-      .find(`[data-ouia-component-type="PF5/Button"]`, 
-          {expanded: false})
-      .first()
-      .as("tgl_details")
-      .click();
+    // cy.get("@table_row")
+    //   .find(`[data-ouia-component-type="PF5/Button"]`, 
+    //       {expanded: false})
+    //   .first()
+    //   .as("tgl_details")
+    //   .click();
 
-    cy.get("@expandable_rows")
-      .first()
-      .should("be.visible");
+    // cy.get("@expandable_rows")
+    //   .first()
+    //   .should("be.visible");
 
-    // cluster config input
-    cy.get("@expandable_rows")
-      .first()
-      .find(`[data-ouia-component-id="metadata-table"]`)
-      .should("be.visible");
+    // // cluster config input
+    // cy.get("@expandable_rows")
+    //   .first()
+    //   .find(`[data-ouia-component-id="metadata-table"]`)
+    //   .should("be.visible");
 
-    // close expandable row
-    cy.get("@tgl_details")
-      .click();
-    cy.get("@expandable_rows")
-      .should("not.be.visible");
+    // // close expandable row
+    // cy.get("@tgl_details")
+    //   .click();
+    // cy.get("@expandable_rows")
+    //   .should("not.be.visible");
   });
 });
