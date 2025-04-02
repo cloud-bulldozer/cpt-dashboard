87e26cc8756d0f5734fda6ac711291fcddd30a35
Matthew F Leader <mleader@redhat.com>
2025-04-02 10:24:41 -0400

    cleanup
    Signed-off-by: Matthew F Leader <mleader@redhat.com>

--- backend/tests/e2e_backend.sh
+++ backend/tests/e2e_backend.sh
@@ -4,13 +4,13 @@ set -e
 echo "building backend test image"
 podman build -f backend/tests/e2e_backend.containerfile --tag e2e-backend ./backend
 
-POD_NAME="pod_e2e"
+export POD_NAME="pod_e2e"
 
 echo "cleaning up and recreating pod"
 podman pod rm -f ${POD_NAME}
 podman pod create --name=${POD_NAME} --publish 8000:8000
 
-POD_NAME="pod_e2e" ./backend/tests/opensearch_ocp.sh
+./backend/tests/opensearch_ocp.sh
 podman run -d --pod=${POD_NAME} --name=back e2e-backend
 echo "seeding db"
 podman exec back poetry run python tests/db_seed.py
