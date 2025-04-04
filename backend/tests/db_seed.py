e7cdb46a39cfdb1e3214432fa2e1ac614563a2e1
Matthew F Leader <mleader@redhat.com>
2025-04-04 14:15:02 -0400

    add functional e2e gui test
    Signed-off-by: Matthew F Leader <mleader@redhat.com>

---
+++ backend/tests/db_seed.py
@@ -0,0 +1,107 @@
+import time
+
+from urllib.parse import urlparse
+from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import TransportError, ConnectionError
+from vyper import v
+
+
+CONTAINER_PATH_SNAPSHOT = "/var/tmp/snapshot"
+
+
+def get_config():
+    v.set_config_name("ocpperf_test")
+    v.add_config_path(".")
+    v.read_in_config()
+    return v
+
+
+def seed_db(srch_client):
+    repo_name = "my_repository"
+    snapshot_name = "my_snapshot"
+    repo_body = {
+      "type": "fs",
+      "settings": {
+        "location": CONTAINER_PATH_SNAPSHOT
+      }
+    }
+    response = srch_client.snapshot.create_repository(
+      repository=repo_name, body=repo_body
+    )
+    print(f"create repo: {response}")
+    assert response["acknowledged"] is True
+    response = srch_client.snapshot.restore(
+      repository=repo_name, snapshot=snapshot_name
+    )
+    print(f"restore snapshot: {response}")
+    assert response["accepted"] is True
+    return True
+
+
+def search_client(
+        host: str = "localhost", port: int = "9200", 
+        username: str = "admin", password: str = "admin"):
+    return Elasticsearch(
+        hosts = [{"host": host, "port": port}],
+        http_compress = True,
+        http_auth = (username, password),
+        use_ssl = False,
+        verify_certs = False,
+        ssl_assert_hostname = False,
+        ssl_show_warn = False
+    )
+
+
+def ocp_index_hits(client_, ocp_index):
+    response = client_.search(
+       index=ocp_index,
+       body={
+          "query": {
+             "match_all": {}
+          }
+        }
+    )
+    if "hits" in response and "total" in response["hits"]:
+       return response["hits"]["total"]["value"]
+    return -1
+
+
+def main():
+    ok = False
+    start = time.time()
+    cfg = get_config()
+    url = urlparse(cfg.get("ocp.elasticsearch.url"))
+    seeded = False
+
+    while not ok:
+      try:
+        client_ = search_client(
+            host=url.hostname,
+            port=url.port,
+            username=cfg.get("ocp.elasticsearch.username"),
+            password=cfg.get("ocp.elasticsearch.password")
+        )
+        response = client_.cluster.health(wait_for_status="yellow")
+        print(f"cluster health: {response['status']}")
+
+        if not seeded:
+           seed_db(client_)
+           seeded = True
+
+        response = client_.indices.get_alias(index="*")
+        print(f'test data index found: {cfg.get("ocp.elasticsearch.indice") in response.keys()}')
+        print(f'ocp index has 273 hits: {ocp_index_hits(client_, cfg.get("ocp.elasticsearch.indice")) == 273}')       
+
+        ok = True
+        print(f"Opensearch ready after {time.time()-start:.3f} seconds")
+        
+      except (
+            TransportError,
+            ConnectionError,
+      ) as exc:
+        print(f"Opensearch isn't ready: {str(exc)!r}")
+        time.sleep(3)
+
+
+if __name__ == "__main__":
+    main()
---
+++ frontend/cypress/e2e/ocp.cy.js
@@ -0,0 +1,99 @@
+describe('ocp user journey', () => {
+  beforeEach(() => {
+    cy.visit("/ocp");
+    cy.findByTestId("side_menu_options")
+      .should("be.visible")
+      .within(() => {
+        cy.findByText("OCP")
+          .should("be.visible")
+          .click();
+      });
+    cy.findByTestId("main_layout_toggle")  
+      .click();
+    cy.findByTestId("side_menu_options")
+      .should("not.be.visible");
+  });
+
+  it("expands a table row's details row to display cluster config metadata table", () => {
+    const start_date = "2025-03-01"
+    const end_date = "2025-03-30"
+
+    // find date filter inputs
+    cy.findByTestId("data_table_filter")      
+      .find(`[ouiaid="date_filter"]`)
+      .within(($date_filter) => {
+        cy.wrap($date_filter)
+          .find(`[class="react-date-picker__inputGroup"]`)
+          .as("date_pickers_inputs");
+      });
+    
+    // set start date
+    // by convention, the first date input on the left receives the start date
+    cy.get("@date_pickers_inputs")
+      .first()
+      .within(($date_pickers) => {
+        cy.wrap($date_pickers)
+          .get("input", {hidden: true})
+          // the date value is saved within the hidden element
+          // in this date picker group
+          .get(":hidden")
+          .type(start_date, {force: true});
+      });
+
+    // set end date
+    // by convention, the next date input (last out of two) receives the end date
+    cy.get("@date_pickers_inputs")
+      .last()
+      .within(($date_pickers) => {
+        cy.wrap($date_pickers)
+          .get("input", {hidden: true})
+          // the date value is saved within the hidden element 
+          // in this date picker group
+          .get(":hidden")
+          .type(end_date, {force: true});
+      }); 
+    
+    // cy.screenshot("main-data-table");
+    cy.screenshot();
+
+    cy.findByTestId("main_data_table")
+      .get("tbody")
+      .find(`[data-ouia-component-type="PF5/TableRow"]`, 
+          {hidden: false})
+      .as("table_row")
+      .first();
+
+    // in the list of table row elements, each table row is 
+    // paired with a subsequent hidden row that can be expanded
+    // to display details
+    cy.get("@table_row")
+      .next()
+      .as("expandable_rows")        
+      .should("not.be.visible");
+
+    cy.get("@table_row")
+      .find(`[data-ouia-component-type="PF5/Button"]`, 
+          {expanded: false})
+      .first()
+      .as("tgl_details")
+      .click();
+
+    cy.get("@expandable_rows")
+      .first()
+      .should("be.visible");
+
+    // cluster config input
+    cy.get("@expandable_rows")
+      .first()
+      .find(`[data-ouia-component-id="metadata-table"]`)
+      .should("be.visible");
+
+    cy.screenshot();      
+
+    // close expandable row
+    cy.get("@tgl_details")
+      .click();
+    cy.get("@expandable_rows")
+      .should("not.be.visible");
+  });
+});
