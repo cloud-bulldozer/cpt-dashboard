783ce70b8eeee05fb5fb8692b5ef2a8aa90d93b2
David Butenhof <dbutenho@redhat.com>
2025-04-16 16:50:06 -0400

    Introduce a CPT backend version
    Version information is compiled at build time using a Python script, producing
    a transient JSON file that will be copied into the backend container for
    access at runtime via `GET /api/version`.

--- backend/tests/e2e_backend.sh
+++ backend/tests/e2e_backend.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e
 
+TOP=$(git rev-parse --show-toplevel)
+BACKEND=${TOP}/backend
+cd ${BACKEND}
+
+echo "generating version file"
+scripts/version.py
+
 echo "building backend image"
-podman build -f backend/backend.containerfile --tag backend ./backend
+podman build -f backend.containerfile --tag backend .
 
 echo "building backend functional test image"
-podman build -f backend/tests/functional.containerfile --tag functional ./backend
+podman build -f tests/functional.containerfile --tag functional .
 
 export POD_NAME="e2e"
 
@@ -13,10 +20,10 @@ echo "cleaning up and recreating pod"
 podman pod rm -f ${POD_NAME}
 podman pod create --name=${POD_NAME} --publish 127.0.0.1:8000:8000 --publish 127.0.0.1:3000:3000
 
-./backend/tests/opensearch_ocp.sh
+./tests/opensearch_ocp.sh
 echo "seeding db"
 podman run --rm --pod=${POD_NAME} --entrypoint python3 functional tests/db_seed.py
 echo "deploying backend"
 podman run -d --pod=${POD_NAME} \
-  -v "$(pwd)/backend/tests/ocpperf_test.toml:/backend/ocpperf.toml:z" \
+  -v "${BACKEND}/tests/ocpperf_test.toml:/backend/ocpperf.toml:z" \
   --name=back backend
