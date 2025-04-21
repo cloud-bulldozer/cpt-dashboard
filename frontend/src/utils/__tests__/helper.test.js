c1c064beb226eef384534dae9f50b5f8fc658f27
Matthew F Leader <mleader@redhat.com>
2025-04-21 17:17:30 -0400

    simplify test cases
    Signed-off-by: Matthew F Leader <mleader@redhat.com>

--- frontend/src/utils/__tests__/helper.test.js
+++ frontend/src/utils/__tests__/helper.test.js
@@ -3,17 +3,11 @@ import { describe, it, expect } from "vitest";
 import { formatDate } from "../helper";
 
 describe("formatDate", () => {
-  it("month and day strings each are length 2", () => {
-    expect(formatDate("1000-10-10")).toBe("1000-10-10");
+  it("month string and day string are length 1", () => {
+    expect(formatDate("1000-9-7")).toBe("1000-09-07");
   });
-  it("month string is length 1", () => {
-    expect(formatDate("1000-9-10")).toBe("1000-09-10");
-  });
-  it("day string is length 1", () => {
-    expect(formatDate("1000-10-9")).toBe("1000-10-09");
-  });
-  it("year string is length 2", () => {
-    expect(formatDate("01-10-09")).toBe("2009-01-10");
+  it("ensure day is not off by one", () => {
+    expect(formatDate("1000-09-08")).toBe("1000-09-08");
   });
   it("error on invalid date string", () => {
     const invalidDate = "dee-doo-doo";
