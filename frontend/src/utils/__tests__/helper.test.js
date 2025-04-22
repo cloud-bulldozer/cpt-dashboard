b906a4485e9597fdaad5ef150d3e48e1cbb0b0f2
Matthew F Leader <mleader@redhat.com>
2025-04-22 08:05:35 -0400

    rm test
    Signed-off-by: Matthew F Leader <mleader@redhat.com>

--- frontend/src/utils/__tests__/helper.test.js
+++ frontend/src/utils/__tests__/helper.test.js
@@ -6,9 +6,6 @@ describe("formatDate", () => {
   it("month string and day string are length 1", () => {
     expect(formatDate("1000-9-7")).toBe("1000-09-07");
   });
-  it("ensure day is not off by one", () => {
-    expect(formatDate("1000-09-08")).toBe("1000-09-08");
-  });
   it("error on invalid date string", () => {
     const invalidDate = "dee-doo-doo";
     expect(() => formatDate(invalidDate))
