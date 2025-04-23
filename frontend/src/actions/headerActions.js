eb80bceb86cab13180fbd5721e49349818400660
David Butenhof <dbutenho@redhat.com>
2025-04-23 16:47:09 -0400

    Add version display to UI (#201)
    * Add version display to UI
    
    This change displays the associated aggregator (backend) version information
    in the header.
    
    NOTE: we may want to associate a separate version with the UI, and display
    that as well?
    
    * We've released v0.1.0; upgrade to v0.1.1
    
    * Oops ... broke the GitHub deploy actions.

--- frontend/src/actions/headerActions.js
+++ frontend/src/actions/headerActions.js
@@ -1,6 +1,26 @@
-import { SET_LAST_UPDATED_TIME } from "./types";
+import * as API_ROUTES from "@/utils/apiConstants";
+import * as TYPES from "./types.js";
+import API from "@/utils/axiosInstance";
+import { showFailureToast } from "./toastActions.js";
 
 export const setLastUpdatedTime = () => ({
-  type: SET_LAST_UPDATED_TIME,
+  type: TYPES.SET_LAST_UPDATED_TIME,
   payload: new Date().toLocaleString().replace(", ", " ").toString(),
 });
+
+export const fetchAggregatorVersion = () => async (dispatch) => {
+  try {
+    dispatch({ type: TYPES.LOADING });
+    const response = await API.get(API_ROUTES.AGG_VERSION_API);
+
+    if (response.status === 200) {
+      dispatch({
+        type: TYPES.SET_AGGREGATOR_VERSION,
+        payload: response?.data
+      });
+    }
+  } catch (error) {
+    dispatch(showFailureToast());
+  }
+  dispatch({ type: TYPES.COMPLETED });
+};
