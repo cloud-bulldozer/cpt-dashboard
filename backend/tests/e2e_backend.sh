17e583abd116b75e728f90533ad9640db672751a
Matthew F Leader <mleader@redhat.com>
2025-05-08 15:31:47 -0400

    add e2e backend script changes
    Signed-off-by: Matthew F Leader <mleader@redhat.com>

--- backend/tests/e2e_backend.sh
+++ backend/tests/e2e_backend.sh
@@ -16,23 +16,14 @@ podman build -f tests/functional.containerfile --tag functional .
 
 export POD_NAME=${POD_NAME:-e2e}
 
-pod_net=${POD_NAME:-e2e}
-podman network remove --force $pod_net
-podman network create $pod_net
-
-
-./tests/opensearch_ocp.sh
-
 echo "cleaning up and recreating pod"
 podman pod rm -f ${POD_NAME}
+podman pod create --name=${POD_NAME} --publish 127.0.0.1:8000:8000 --publish 127.0.0.1:3000:3000
 
-
+./tests/opensearch_ocp.sh
+echo "seeding db"
+podman run --rm --pod=${POD_NAME} --entrypoint python3 functional tests/db_seed.py
 echo "deploying backend"
-podman run -d --pod=new:${POD_NAME}-pod-back \
-  --network=${pod_net} \
-  --publish 127.0.0.1:8000:8000 --publish 127.0.0.1:3000:3000 \
+podman run -d --pod=${POD_NAME} \
   -v "${BACKEND}/tests/ocpperf_test.toml:/opt/backend/ocpperf.toml:z" \
   --name="${POD_NAME}-back" backend
-
-echo "seeding db"
-podman run --rm --pod=${POD_NAME}-pod-back --network=${pod_net} --entrypoint python3 functional tests/db_seed.py
