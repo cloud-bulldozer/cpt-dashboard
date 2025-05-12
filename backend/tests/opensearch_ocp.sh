2d418dec47868749f1c2dc8a6d0c12dbe3f1c869
David Butenhof <dbutenho@redhat.com>
2025-05-12 11:44:13 -0400

    Fix ILAB Opensearch mappings
    The original ILAB functional test clone snapshot was created without proper
    document mappings, resulting in failure of aggregations for summary and graph
    data. This PR replaces the snapshot with a new repaired version.
    
    This includes a one-time "fix_snapshot.py" script mostly to capture the
    process and history for posterity. (It shouldn't be needed again.)
    
    I've also moved the snapshot into the same location as the OCP snapshot for
    consistency. I also renamed the OCP snapshot to avoid confusion, but changes
    to the frontend e2e flow are minimal.

--- backend/tests/opensearch_ocp.sh
+++ backend/tests/opensearch_ocp.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ex # Fail on error
 BRANCH=$(git rev-parse --show-toplevel)
-SETUP=${BRANCH}/backend/tests
+TESTS=${BRANCH}/backend/tests
+SNAPSHOT=${TESTS}/fixtures/search_db_snapshots
+SETUP=${TESTS}/functional/setup
 
 
 if [[ -n "${POD_NAME}" ]] ;then
@@ -18,13 +20,13 @@ fi
 podman rm -f ${NAME}
 
 podman run -d ${POD} --name "${NAME}" \
-    -v "${SETUP}"/functional/setup/ocp_opensearch.yml:/usr/share/opensearch/config/opensearch.yml:z \
+    -v "${SETUP}"/ocp_opensearch.yml:/usr/share/opensearch/config/opensearch.yml:z \
     ${PORTS} \
     -e "discovery.type=single-node" -e "DISABLE_INSTALL_DEMO_CONFIG=true" \
     -e "DISABLE_SECURITY_PLUGIN=true" \
     --pull=always \
     docker.io/opensearchproject/opensearch:latest
-podman cp ${SETUP}/fixtures/search_db_snapshots/snapshot.tar.gz ${NAME}:/var/tmp/snapshot.tar.gz
+podman cp ${SNAPSHOT}/ocp-snapshot.tar.gz ${NAME}:/var/tmp/snapshot.tar.gz
 echo "Unpacking snapshot inside container"
 podman exec "${NAME}" bash -c 'cd /var/tmp ; tar xfz snapshot.tar.gz'
 echo "Done"
