98fdf89bd03a5e78ff399fed75ad040873c940d0
Varshini M <varm@redhat.com>
2025-05-13 17:54:42 +0530

    PANDA-844 To enchance the filter usability
    Signed-off-by: Varshini M <varm@redhat.com>

---
+++ frontend/src/components/molecules/EmptyState/index.jsx
@@ -0,0 +1,30 @@
+import {
+  EmptyState,
+  EmptyStateBody,
+  EmptyStateHeader,
+  EmptyStateIcon,
+} from "@patternfly/react-core";
+
+import PropTypes from "prop-types";
+import SearchIcon from "@patternfly/react-icons/dist/esm/icons/search-icon";
+
+const msgToShow = {
+  noData: "No data found for the selected date range.",
+  noFilterData: "No results match the filter criteria.",
+};
+const CustomEmptyState = ({ type }) => (
+  <EmptyState>
+    <EmptyStateHeader
+      titleText="No results found"
+      headingLevel="h4"
+      icon={<EmptyStateIcon icon={SearchIcon} />}
+    />
+    <EmptyStateBody>{msgToShow[type]}</EmptyStateBody>
+  </EmptyState>
+);
+
+CustomEmptyState.propTypes = {
+  type: PropTypes.string,
+};
+
+export default CustomEmptyState;
