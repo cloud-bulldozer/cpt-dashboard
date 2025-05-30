854342cbc6c52a6b2c3dd115e5d9a291b2eb34d6
David Butenhof <dbutenho@redhat.com>
2025-05-30 15:21:25 -0400

    Consolidate test setup
    The frontend and backend functional tests use completely different deployment
    strategies, which complicates maintenance. While the frontend's "podman
    compose" mechanism is appealing, it only works with docker *also* installed
    as `podman compose` preferentially uses the docker compose engine and the
    podman compose engine doesn't function correctly.
    
    Instead, consolidate on a simpified manual podman pod deployment strategy
    using a common helper script. I moved some files around to help make this a
    bit more consistent and readable: it'd be nice if GitHub were smart enough
    to recognize the moved files, but I'm not sure it will.

--- backend/tests/functional/setup/test.sh
+++ backend/tests/functional/setup/test.sh
@@ -1,47 +1,11 @@
 #!/bin/bash
 if [ ${DEBUG} ]; then set -ex ;fi
 
-cleanup () {
-    set +e
-    echo "Cleaning up..."
-    podman pod stop "${POD_NAME}"
-    podman rm "${POD_NAME}-func" "${POD_NAME}-front" "${POD_NAME}-back" "${POD_NAME}-opensearch"
-    podman pod rm "${POD_NAME}"
-}
-
 BRANCH="$(git rev-parse --show-toplevel)"
-BACKEND="${BRANCH}/backend"
-FRONTEND="${BRANCH}/frontend"
-SETUP="${BACKEND}"/tests/functional/setup
-CPT_CONFIG=${CPT_CONFIG:-"${SETUP}/funcconfig.toml"}
-export POD_NAME=${POD_NAME:-FUNC${RANDOM}}
+TESTING="${BRANCH}/testing"
 
-# DEVEL=1 backend/tests/functional/setup/test.sh
-#
-# In "DEVEL" mode, allow external access to the server port, and don't terminate
-# the pod when this script exits.
-if [ ${DEVEL} ]
-then
-    PUBLISH="--publish 127.0.0.1:8000:8000 --publish 127.0.0.1:9200:9200"
-else
-    PUBLISH=""
-    trap cleanup EXIT
-fi
-podman pod create --name=${POD_NAME} ${PUBLISH}
+. ${TESTING}/pod_setup.sh
+CONTAINERS+=( "${POD_NAME}-func" )
 
-${BACKEND}/scripts/version.py
-podman build -f backend.containerfile --tag backend "${BACKEND}"
-podman build -f frontend.containerfile --tag frontend "${FRONTEND}"
-podman build -f tests/functional/setup/functional.containerfile --tag functional "${BACKEND}"
-
-POD="--pod ${POD_NAME}"
-
-"${SETUP}"/opensearch.sh
-podman run -d ${POD} --name="${POD_NAME}-back" -v "${CPT_CONFIG}:/opt/backend/ocpperf.toml:Z" localhost/backend
-podman run -d ${POD} --name="${POD_NAME}-front" localhost/frontend
+podman build -f ${TESTING}/functional.containerfile --tag functional "${BRANCH}"
 podman run ${POD} --name="${POD_NAME}-func" localhost/functional
-
-if [ ${DEVEL} ]
-then
-    echo -e "\n\n--------------\nWhen finished, type\n  podman pod stop ${POD_NAME}\n--------------"
-fi
--- testing/opensearch.sh
+++ testing/opensearch.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 set -ex # Fail on error
 BRANCH=$(git rev-parse --show-toplevel)
-TESTS=${BRANCH}/backend/tests
-SNAPSHOT=${TESTS}/fixtures/search_db_snapshots
-SETUP=${TESTS}/functional/setup
+TESTING=${BRANCH}/testing
+COMPONENT=${COMPONENT:-ocp}
+
+OSPORT=${OPENSEARCH_PORT:-9200}
+NAME=${OPENSEARCH_NAME:-opensearch-${COMPONENT}}
 
 if [[ -n "${POD_NAME}" ]] ;then
     echo "Running in POD ${POD_NAME}"
     POD="--pod ${POD_NAME}"
+    NAME="${POD_NAME}-${NAME}"
     PORTS=
-    NAME="${POD_NAME}-opensearch"
 else
     POD=""
-    PORTS="-p 9200:9200 -p 9600:9600"
-    NAME="opensearch"
+    PORTS="-p ${OSPORT}:${OSPORT}"
+fi
+
+if [ "${OSPORT}" != "9200" ] ;then
+    PORTENV="-e http.port=${OSPORT}"
+else
+    PORTENV=
 fi
 
 podman run -d ${POD} --name "${NAME}" \
-    -v "${SETUP}"/opensearch.yml:/usr/share/opensearch/config/opensearch.yml:z \
-    -v "${SNAPSHOT}"/ilab-snapshot.tar.gz:/var/tmp/snapshot.tar.gz:z \
+    -v "${TESTING}"/opensearch.yml:/usr/share/opensearch/config/opensearch.yml:z \
+    -v "${TESTING}"/${COMPONENT}/snapshot.tar.gz:/var/tmp/snapshot.tar.gz:z \
     ${PORTS} \
     -e "discovery.type=single-node" -e "DISABLE_INSTALL_DEMO_CONFIG=true" \
-    -e "DISABLE_SECURITY_PLUGIN=true" \
+    -e "DISABLE_SECURITY_PLUGIN=true" ${PORTENV} \
     docker.io/opensearchproject/opensearch:latest
 echo "Unpacking snapshot inside container"
 podman exec "${NAME}" bash -c 'cd /var/tmp ; tar xfz snapshot.tar.gz'
