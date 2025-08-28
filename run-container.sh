999301865f23a4f93a9b88235827a3c43eb25b23
David Butenhof <dbutenho@redhat.com>
2025-08-28 16:24:39 -0400

    Validate OpenSearch server certificates
    The CPT dashboard has been accessing most of the OpenSearch datastores using TLS, but with `verify_cert=False`, eliminating most of the security value of having TLS. This change adjusts the backend container build to include the Red Hat CA chain so that we can verify connections to internal datastores, and drops the use of `verify_cert=False`.
    
    This implies a change to the secrets; one of the INTLAB OpenSearch servers (used by HCE) has been identified using a URL that doesn't have a valid certificate--however it has an alias URL that does validate. I've also considered that there may be a need to expose a TOML configuration option to suppress certificate validation for specific servers: however that's an undesirable configuration from a security point of view, and we can worry about that later if it proves necessary.
    
    The interesting thing is that Red Hat considers the Red Hat CA cert to be internal-only information, which means we don't want to put the URL into GitHub. Therefore, the GitHub Actions now expect a `CA_CERT_PATH` *secret* to be available on the repo, which will be added into the Quay container builds; the cert path isn't needed for the functional tests, which (currently) use local non-TLS unsigned container OpenSearch instances.
    
    This also adds a `run-container.sh` script to build an run a backend container locally in order to test the container build (and CA chain insertion). This requires passing a `CA_CERT_PATH` URL as an environment variable. Under most circumstances, `run-local.sh` is sufficient for testing.

---
+++ run-container.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Build and run a backend container for testing.
+#
+# NOTE:
+# This does not build or run the frontend. That would require additional
+# framework to work around the NGINX dependency on the kubernetes backend
+# service.
+if [ ${DEBUG} ]; then set -ex ;fi
+
+BRANCH="$(git rev-parse --show-toplevel)"
+BACKEND="${BRANCH}/backend"
+FRONTEND="${BRANCH}/frontend"
+CPT_CONFIG=${CPT_CONFIG:-"${BACKEND}/ocpperf.toml"}
+
+export CONTAINERS=()
+
+cleanup () {
+    set +e
+    if [[ "${NOCLEANUP}" -ne 1 && "${#CONTAINERS[@]}" -ne 0 ]]
+    then
+        echo "Cleaning up..."
+        podman stop "${CONTAINERS[@]}"
+        podman rm "${CONTAINERS[@]}"
+    else
+        echo "Leaving pod ${POD_NAME} running for debug"
+    fi
+}
+
+if [[ -z "${CA_CERT_PATH}" ]]; then
+    echo "CA_CERT_PATH is not set to a valid CA certificate path"
+    exit 1
+fi
+
+echo "Creating version"
+( cd ${BACKEND}; poetry install ; poetry run scripts/version.py )
+podman build -f backend.containerfile --build-arg CA_CERT_PATH="${CA_CERT_PATH}" --tag backend "${BACKEND}"
+echo "Starting backend container"
+podman run -d --name="backend" -p 127.0.0.1:8000:8000 -v "${CPT_CONFIG}:/backend/ocpperf.toml:Z" localhost/backend
+CONTAINERS=( "backend" )
+
+echo -e "\n\n--------------\nWhen finished, type\n  podman rm --force ${CONTAINERS[@]}\n--------------"
